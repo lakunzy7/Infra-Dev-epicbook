1a3fe00a8660ba462b4bccbb471de373a0e7cf8b
Lakunzy7 <lakunzy7@gmail.com>
2025-11-07 16:18:06 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.51.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  subscription_id = "1b6b3c06-06ea-4bfc-93a5-14d138d19ff2"
+
+  subscription_id = "8***************************************"
 }
